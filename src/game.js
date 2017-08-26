a669355d63ac44b0416ee1e5e5543bed699afebe
bad4iz <bad4iz@gmail.com>
2017-08-26 11:08:43 +0400

    №2

---
+++ src/game.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          board
+        </div>
+        <div>
+        </div>
+        <h1> Hello Game!!!</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
