54cf0c9cd622adc4814b93fda4fc5b0a64c42bc8
bad4iz <bad4iz@gmail.com>
2017-08-26 16:51:20 +0400

    aa

---
+++ src/board.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Cell from "./cell";
+
+class Board extends Component {
+
+    renderCell(i){
+        return <Cell value={i} />
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderCell(0)}
+                    {this.renderCell(1)}
+                    {this.renderCell(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderCell(3)}
+                    {this.renderCell(4)}
+                    {this.renderCell(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderCell(6)}
+                    {this.renderCell(7)}
+                    {this.renderCell(8)}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Board;
